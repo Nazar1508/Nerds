5be35360350c67bb8550099231d13a4537efc9c2
Nazar1508 <nazar-koltsun@ukr.net>
2018-01-15 22:31:22 +0200

    Версія 23
    Java script,  добавляємо карту

--- js/script.js
+++ js/script.js
@@ -1,7 +1,21 @@
- var link = document.querySelector(".button-address");
- var popup = document.querySelector(".modal-login");
+var link = document.querySelector(".button-address");
+var popup = document.querySelector(".modal-login");
+var close = popup.querySelector(".modal-close");
  
- link.addEventListener("click", function (evt) {
- 	evt.preventDefault();
- 	popup.classList.add("modal-show");
-  });
+link.addEventListener("click", function (evt) {
+	evt.preventDefault();
+	popup.classList.add("modal-show");
+});
+
+close.addEventListener("click", function (evt) {
+	evt.preventDefault();
+	popup.classList.remove("modal-show");
+});
+
+window.addEventListener("keydown", function (evt) {
+if (evt.keyCode === 27) {
+  if (popup.classList.contains("modal-show")) {
+    popup.classList.remove("modal-show");
+  }
+}
+});
